997e225d9e302b62a4fcc0e32492672f4bac0479
DK <66110819+LegeApp@users.noreply.github.com>
2026-02-02 13:12:16 -0800

    Delete src directory

--- src/annotations/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod annotations;
-pub mod hidden_text;
-pub mod string;
-
-pub use annotations::{Annotations, Hyperlink, AnnotationShape};
-pub use hidden_text::HiddenText;
--- src/encode/zp/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod table;
-#[path = "ZPcodec.rs"]
-pub mod zp_codec;
-
-pub use zp_codec::*;
